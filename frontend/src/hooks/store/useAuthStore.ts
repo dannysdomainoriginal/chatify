a1082fdab7295ab6bab575bddb793c5bc142181a
Charles Daniel <dannysdomainoriginal@gmail.com>
2025-12-22 14:21:00 +0100

    Initial Frontend setup

---
+++ frontend/src/hooks/store/useAuthStore.ts
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+
+// TODO: watch that Zustand best practices video
+// Learn how to make interfaces for stores
+
+const useAuthStore = create((set, get) => ({
+
+}))
