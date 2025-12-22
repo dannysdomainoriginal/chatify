304a1d37e6740384474f1fed30ffb7f88537d490
Charles Daniel <dannysdomainoriginal@gmail.com>
2025-12-22 09:41:13 +0100

    Done with backend setup & routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,13 @@ import routes from "./routes/index.route";
 import { join } from "path";
 import { connectDB } from "./lib/db";
 import parser from "cookie-parser"
+import morgan from "morgan"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(parser())
+app.use(morgan("dev"))
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50kb" }));
 
